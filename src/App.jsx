84912a80396fa98ae86cdd32777ca56d593cd176
DPEDITS <debashishparida75@gmail.com>
2025-04-07 19:42:03 +0530

    modified with footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Library from './components/Library'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
     <Library/>
     </div>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../components/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer-left'>
+        <h1 className='f1'>Preview Of Spotify</h1>
+        <p className='f2'>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed</p>
+      </div>
+      <button className="text-button5">Sign Up</button>
+    </div>
+  )
+}
+
+export default Footer
